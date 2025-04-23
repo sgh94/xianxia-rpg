d1c73df8c5e9015e1fc5172d84e66edac183d20f
sgh94 <vpvpvhvp98@gmail.com>
2025-04-24 00:37:48 +0900

    tsc

--- next.config.js
+++ next.config.js
@@ -3,7 +3,11 @@ const { i18n } = require('./next-i18next.config');
 
 const nextConfig = {
   reactStrictMode: true,
-  i18n,
+  i18n: {
+    locales: ['en', 'ko'], // 사용 중인 로케일 목록 예시
+    defaultLocale: 'en',   // 기본 로케일 예시
+    localeDetection: false, // <--- 이 값을 false 로 설정하세요.
+  },
   env: {
     GEMINI_API_URL: process.env.GEMINI_API_URL,
     GEMINI_API_KEY: process.env.GEMINI_API_KEY,
